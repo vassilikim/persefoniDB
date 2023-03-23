118b776ae3b752f6d7fc5a1771f7d2bee1d510f6
Vassiliki M <missirivassiliki@gmail.com>
2023-03-23 22:39:13 +0200

    fixed bug

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -2,10 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const config = {
-  host: "localhost",
-  user: "root",
-  password: "hellobts1234",
-  database: "sakila",
+  host: process.env.DATABASE_SERVER,
+  user: process.env.DATABASE_INSTANCENAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 };
 
 module.exports = config;
