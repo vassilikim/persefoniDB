4a7cad84db20f955ead4e86a53789bf899ddfc73
Vassiliki M <missirivassiliki@gmail.com>
2023-03-29 13:03:59 +0300

    added protect and restrictTo

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,9 +2,18 @@ var config = require("../dbconfig");
 const sql = require("mysql");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
+const { promisify } = require("util");
 
 correctPassword = async function (candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+changedPasswordAfter = function (JWTTimestamp, passwordChangedAt) {
+  if (passwordChangedAt) {
+    const changedTimestamp = parseInt(passwordChangedAt.getTime() / 1000, 10);
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 
 exports.signup = async (req, res) => {
@@ -105,70 +114,89 @@ exports.login = async (req, res, next) => {
   }
 };
 
-// exports.protect = async (req, res, next) => {
-//   try {
-//     let token;
-//     if (
-//       req.headers.authorization &&
-//       req.headers.authorization.startsWith("Bearer")
-//     ) {
-//       token = req.headers.authorization.split(" ")[1];
-//     }
+exports.protect = async (req, res, next) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
 
-//     if (!token) {
-//       return res.status(401).json({
-//         status: "failed",
-//         message: "Please log in to get access.",
-//       });
-//     }
+    if (!token) {
+      return res.status(401).json({
+        status: "failed",
+        message: "Please login to get access.",
+      });
+    }
 
-//     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-//     let pool = await sql.connect(config);
-//     let customer = await pool
-//       .request()
-//       .input("Id", sql.UniqueIdentifier, decoded.id)
-//       .query("SELECT * FROM tblcustomer WHERE Id=@Id");
-//     customer = customer.recordsets[0];
+    let connection = sql.createConnection(config);
+    connection.connect();
 
-//     if (customer.length == 0) {
-//       return res.status(401).json({
-//         status: "failed",
-//         message: "User no longer exists.",
-//       });
-//     }
+    connection.query(
+      `SELECT * FROM Users WHERE username='${decoded.id}'`,
+      function (error, results, fields) {
+        if (error)
+          return res.status(500).json({
+            status: "failed",
+            message: error.message,
+          });
 
-//     // if (freshUser.changedPasswordAfter(decoded.iat)) {
-//     //   responseMessage = {
-//     //     status: "failed",
-//     //     message: "User recently changed password. Please log in again.",
-//     //   };
-//     //   return handleResponse(req, res, 401, handleResponse);
-//     // }
+        if (results.length == 0) {
+          return res.status(401).json({
+            status: "failed",
+            message: "User no longer exists.",
+          });
+        }
+        let freshUser = results[0];
 
-//     req.email = customer[0].emailaddress;
-//     req.pelperi = customer[0].peri;
-//     req.phone = customer[0].phone;
-//     req.mobile = customer[0].mobile;
-//     req.pelid = customer[0].id;
+        if (
+          changedPasswordAfter(decoded.iat, freshUser["changed_password_at"])
+        ) {
+          return res.status(401).json({
+            status: "failed",
+            message:
+              "User recently changed their password! Please login again.",
+          });
+        }
+        req.username = freshUser.username;
+        req.role = freshUser.user_role;
+        next();
+      }
+    );
+    connection.end();
+  } catch (err) {
+    if (err.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        status: "failed",
+        message: "Please login to get access.",
+      });
+    } else if (err.name === "TokenExpiredError") {
+      return res.status(401).json({
+        status: "failed",
+        message: "Token has expired. Please login again.",
+      });
+    } else {
+      return res.status(500).json({
+        status: "failed",
+        message: err.message,
+      });
+    }
+  }
+};
 
-//     next();
-//   } catch (err) {
-//     if (err.name === "JsonWebTokenError") {
-//       return res.status(401).json({
-//         status: "failed",
-//         message: "Please login to get access.",
-//       });
-//     } else if (err.name === "TokenExpiredError") {
-//       return res.status(401).json({
-//         status: "failed",
-//         message: "Token has expired. Please login again.",
-//       });
-//     } else {
-//       return res.status(500).json({
-//         status: "failed",
-//         message: err.message,
-//       });
-//     }
-//   }
-// };
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.role)) {
+      console.log(roles);
+      return res.status(403).json({
+        status: "failed",
+        message: "You don't have permission to perform this action!",
+      });
+    }
+    next();
+  };
+};
--- backend/routes/superAdminRoutes.js
+++ backend/routes/superAdminRoutes.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var backupController = require("../controllers/backupController");
+var authController = require("../controllers/authController");
 
 /* GET users listing. */
-router.get("/backup", backupController.backup);
-router.get("/restore", backupController.restore);
+router.get(
+  "/backup",
+  authController.protect,
+  authController.restrictTo("super-admin"),
+  backupController.backup
+);
+router.get(
+  "/restore",
+  authController.protect,
+  authController.restrictTo("super-admin"),
+  backupController.restore
+);
 
 module.exports = router;
